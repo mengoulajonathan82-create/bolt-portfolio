facf551724c59bb03d8de30c4a2c2091e69d85bf
Jonathan mengoula <mengoulajonathan82@gmail.com>
2026-02-28 14:38:47 +0100

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bolt-portfolio',
+  base: '/bolt-portfolio/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
